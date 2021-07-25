6cde2ecf5e077ad729775570929ef46c47905ea7
MuhammadHassanKhattak <72311666+MuhammadHassanKhattak@users.noreply.github.com>
2021-07-25 02:48:18 -0700

    added assignment

---
+++ app.js
@@ -0,0 +1,80 @@
+
+// var Array1 = [
+//     { name: "juice", price: "50", quantity: "3" },
+//     { name: "cookie", price: "30", quantity: "9" },
+//     { name: "shirt", price: "880", quantity: "1" },
+//     { name: "pen", price: "100", quantity: "2" }
+// ];
+
+// var tPrice = 0;
+// for (var i = 0; i < Array1.length; i++) {
+//     var prices = `Total price of ${Array1[i].name} is : ${Array1[i].price * Array1[i].quantity}`
+//     console.log(prices)
+//     tPrice += (+Array1[i].price) * (Array1[i].quantity)
+// }
+// console.log(`Total price of all items: ${tPrice}`)
+
+
+
+
+
+
+
+// var user = {
+//     name: "hassan",
+//     email: "hassan@gmial.com",
+//     password: "4343434",
+//     age: 18,
+//     gender: "male",
+//     city: "karachi",
+//     country: "pakistan"
+// }
+
+// var fData = prompt("fine a property in object");
+// if (user.hasOwnProperty(fData)) {
+//     console.log(`${fData} propert found in object`)
+// } else {
+//     console.log(`${fData} property not found in object`)
+
+// }
+
+
+
+
+// function Userdata(userName, email, password) {
+//     this.userName = userName;
+//     this.email = email;
+//     this.password = password;
+// }
+
+// var InputUser = prompt("inout your name")
+// var InputEmail =prompt("inout your name")
+// var InputPassword1 = prompt("inout your name")
+
+
+// var objArr = []
+
+
+//     var userdata1 = new Userdata(InputUser, InputEmail, InputPassword1)
+
+//     console.log(userdata1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
